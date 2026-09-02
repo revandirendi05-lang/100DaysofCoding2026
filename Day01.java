da0f083a3995693b3fe06c4cab7bcf92d03b2c31
revandy.r05 <revandirendi05@gmail.com>
2026-09-02 20:42:59 +0800

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,19 @@
+/**
+ * Contoh penggunaan komentar dalam Java.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Menampilkan nama pengguna
+        String nama = "Rendi Revandi";
+
+        /*
+         * Kecamatan karossa.
+         * Kabupaten mamuju tengah
+         * Provinsi Sulawesi barat.
+         */
+        System.out.println("Halo, " + nama);
+
+    }
+}
