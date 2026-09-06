ff84d996568081afb25c3f53ff6594ed2152ef22
revandy.r05 <revandirendi05@gmail.com>
2026-09-06 19:46:19 +0800

    Create day05.java

---
+++ day05.java
@@ -0,0 +1,19 @@
+public class Data {
+
+    public static void main(String[] args) {
+        
+        // Tipe data pecahan
+
+        float hasiltim1 = 25.368f;
+        double hasiltim2 = 24.999;
+
+        System.out.println("Hasil Tim 1  =\t" + hasiltim1);
+        System.out.println("Hasil Tim 2  =\t" + hasiltim2);
+
+        // PENJELASAN 
+        /*
+        * float adalah data pecahan dengan jumlah digit presisi adalah 6 sampai 7
+        * sedangkan double 15 hingga 17 digit
+        * */ 
+    } 
+}
