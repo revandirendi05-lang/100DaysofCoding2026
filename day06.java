f4b5be629bc60c6f52377c9d36eaefd5529c73f4
revandy.r05 <revandirendi05@gmail.com>
2026-09-07 22:19:13 +0800

    Create day06.java

---
+++ day06.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Tipe data boolean
+        /*
+        * Boolean adalah sebuah tipe data
+        * yang memiliki dua kemungkinan 
+        * yaitu true and false
+        */
+
+        // CONTOH
+        boolean A = (5>2);
+        boolean B = (9<6);
+
+        System.out.println("Apakah 5 lebih besar dari pada 2 =\t" + A);
+        System.out.println("Apakah 9 lebih kecil dari 6\t =\t" + B);
+
+        // Tipe data char
+        /*
+        * char adalah tipe data dalam pemrograman 
+        * untuk menyimpan satu buah karakter
+        */
+
+        // CONTOH
+        char Rendi = ('R');
+        char angkafavorit = ('5');
+
+        System.out.println("INISIAL\t\t =\t" + Rendi);
+        System.out.println("ANGKA FAVORIT\t =\t" + angkafavorit);
+
+    }
+
+}
