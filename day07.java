985c779897de6628c9ee532778c8a4366780b699
revandy.r05 <revandirendi05@gmail.com>
2026-09-08 19:37:47 +0800

    Create day07.java

---
+++ day07.java
@@ -0,0 +1,22 @@
+public class day7 {
+
+    public static void main(String[] args){
+
+        // TIPE DATA STRING
+        // Adalah tipe data dalam programan yang digunakan unuk menyimpan karakter, simbol, teks, dan anggka
+        
+        // Contoh
+
+        String Karakter = "R";
+        String simbol = "@";
+        String teks = "Hallo Everyone";
+        String Angka = "15";
+
+        System.out.println("String Karakter  =\t"  + Karakter);
+        System.out.println("String Simbol    =\t"  + simbol);
+        System.out.println("String Teks      =\t"  + teks);
+        System.out.println("String Angka     =\t"  + Angka);
+
+    }
+
+}
