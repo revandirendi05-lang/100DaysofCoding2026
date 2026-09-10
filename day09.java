e85e7d4944ba10e6eb367b3fc456bbc8d9717580
revandy.r05 <revandirendi05@gmail.com>
2026-09-10 22:03:05 +0800

    Create day09.java

---
+++ day09.java
@@ -0,0 +1,22 @@
+public class day9 {
+
+    public static void main(String[] args) {
+        
+        // Final Variabel 
+        /* Final variabel adalah variabel yang nilainya hanya dapat diisi satu kali
+        * dan tidak dapat di ubah setelah diinisialisasi
+        */
+
+        final String ketuaorganisasi = "Rendi Revandy";
+        final String maximalanggota = "30 orang";
+        final int batasumur = 18;
+
+        System.out.println("Ketua Organisasi =\t" + ketuaorganisasi);
+        System.out.println("MAX Anggota      =\t" + maximalanggota);
+        System.out.println("Batas Umur       =\t" + batasumur);
+
+        // Jika melebihi atau mengganti variabel yang sudah final sistem akan eror
+
+
+    }
+}
