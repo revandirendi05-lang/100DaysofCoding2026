c312c80e98f093e3d051a9b33b41b2855c8c8dc4
revandy.r05 <revandirendi05@gmail.com>
2026-09-11 21:42:37 +0800

    Create day10.java

---
+++ day10.java
@@ -0,0 +1,60 @@
+public class day10 {
+public static void main(String[] args) {
+    
+/* Byte
+* minimal byte adalah -128
+* sedangkan maksimal byte adalah 127
+ */
+
+System.out.println("BYTE MIN\t =\t" + Byte.MIN_VALUE);
+System.out.println("BYTE MAX\t =\t" + Byte.MAX_VALUE);
+
+/* short
+* minimal short adalah -32768
+* sedangkan maksimal short adalah 32767 
+*/
+
+System.out.println("SHORT MIN\t =\t" + Short.MIN_VALUE);
+System.out.println("SHORT MAX\t =\t" + Short.MAX_VALUE);
+
+/* int
+* minimal int adalah -2147483648
+* sedangkan maksimal int adalah 2147483647
+*/
+
+System.out.println("INT MIN\t\t =\t" + Integer.MIN_VALUE);
+System.out.println("INT MAX\t\t =\t" + Integer.MAX_VALUE);
+
+/* long
+* minimal long adalah minus sembilan eksiliun
+* sedangkan maksimal long adalah sembilan eksiliun
+*/
+
+System.out.println("LONG MIN\t =\t" + Long.MIN_VALUE);
+System.out.println("LONG MAX\t =\t" + Long.MAX_VALUE);
+
+/* float
+* minimal float adalah minus tujuh angka di belakang koma
+* sedangkan maksimal float adalah tujuh angka di belakang koma
+*/
+
+System.out.println("FLOAT MIN\t =\t" + Float.MIN_VALUE);
+System.out.println("FLOAT MAX\t =\t" + Float.MAX_VALUE);
+
+/* double
+* minimal double adalah minus enam belas angka di belkang koma
+* sedangkan maksimal double adalah enam belas angka di belakang koma
+*/
+System.out.println("DOUBLE MIN\t =\t" + Double.MIN_VALUE);
+System.out.println("DOUBLE MAX\t =\t" + Double.MAX_VALUE);
+
+/* char
+* minimal char adalah 0
+* sedangkan maksimal char adalah 65535
+*/
+
+System.out.println("CHAR MIN\t =\t" + (int) Character.MIN_VALUE);
+System.out.println("CHAR MAX\t =\t" + (int) Character.MAX_VALUE);
+
+}    
+}
