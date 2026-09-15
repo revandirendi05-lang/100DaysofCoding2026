9acb2867d4d5a72663549495cba9679209875be6
revandy.r05 <revandirendi05@gmail.com>
2026-09-15 21:49:53 +0800

    Create day14.java

---
+++ day14.java
@@ -0,0 +1,19 @@
+public class day14 {
+    public static void main(String[] args) {
+        // Penjumlahan dan Pengurangan 
+
+        byte angka1 = 15;
+        byte angka2 = 9;
+        byte angka3 = 28;
+
+        int total1 = angka2 + angka3;
+        int total2 = total1 - angka3;
+        int total3 = angka2 + angka3 - angka1;
+
+        System.out.println("Hasil penjumlahan :\t" + total1);
+        System.out.println("Hasil pengurangan :\t" + total2);
+        System.out.println("TOTAL             :\t" + total3);
+   
+   
+    }
+}
