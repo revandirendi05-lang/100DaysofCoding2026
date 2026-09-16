05e98331154966ee1d2c0f16fcef64d252a02037
revandy.r05 <revandirendi05@gmail.com>
2026-09-16 21:16:04 +0800

    Create day15.java

---
+++ day15.java
@@ -0,0 +1,19 @@
+public class day15 {
+    public static void main(String[] args) {
+        // Perkalian dan pembagian 
+
+        byte angka1 = 15;
+        byte angka2 = 9;
+        byte angka3 = 28;
+
+        int total1 = angka1 * angka3;
+        int total2 = total1 / angka2;
+        int total3 = angka1 * angka3 / angka2;
+
+        System.out.println("Hasil perkalian :\t" + total1);
+        System.out.println("Hasil pembagian :\t" + total2);
+        System.out.println("TOTAL           :\t" + total3);
+   
+   
+    }
+}
