92760ce5a0be6e64d86ad64f3d9b5ad489d9492b
revandy.r05 <revandirendi05@gmail.com>
2026-09-17 21:42:05 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,14 @@
+public class day16 {
+    public static void main(String[] args) {
+        // MODULUS
+
+        byte angka1 = 15;
+        byte angka2 = 8;
+
+        int total1 = angka1 % angka2;
+
+        System.out.println("Sisa Bagi :\t" + total1);
+   
+   
+    }
+}
