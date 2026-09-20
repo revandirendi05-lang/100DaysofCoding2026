e26ad23986e9910f70a3d469d68ee7912d744aea
revandy.r05 <revandirendi05@gmail.com>
2026-09-20 22:16:54 +0800

    Create day19.java

---
+++ day19.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class day19 {
+
+    public static void main(String[] args) {
+        
+        Scanner rr = new Scanner(System.in);
+
+        System.out.print("Nilai Tugas :\t");
+        double nilai = rr.nextDouble();
+
+        // KONVERSI
+        byte nilai1 = (byte)nilai;
+
+        System.out.println("Sebelum koversi =\t" + nilai);
+        System.out.println("Sebelum koversi =\t" + nilai1);
+
+    }
+
+}
